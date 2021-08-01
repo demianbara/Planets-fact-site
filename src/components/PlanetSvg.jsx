142bbe0187e35360d12715f06368897d7a8da36a
Demi26 <demianbara@gmail.com>
2021-08-01 16:23:35 -0300

    ipad responsive

--- src/components/PlanetSvg.jsx
+++ src/components/PlanetSvg.jsx
@@ -1,17 +1,16 @@
 import * as React from "react";
 
 export const Web = ({imgType, planetData}) => {
-    return (imgType === "geology" ? (
+    return imgType === "geology" ? (
         <div className="main-planet-img">
             <img className="planet" src={planetData.images["planet"]} />
             <img className="geology" src={planetData.images[imgType]} />
         </div>
     ) : (
         <div className="main-planet-img">
-            <img src={planetData.images[imgType]} />
+            <img className="planet" src={planetData.images[imgType]} />
         </div>
-    )
-    )
+    );
 };
 
 export const Mobile = ({ imgType, planetData }) => {
